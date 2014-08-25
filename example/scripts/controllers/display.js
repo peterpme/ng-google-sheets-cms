353c90dfa7360b3fbd764b0b092049a4e3f71b87
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-08-25 18:37:33 -0500

    updated html & styling

--- example/scripts/controllers/display.js
+++ example/scripts/controllers/display.js
@@ -1,15 +1,17 @@
 'use strict';
 
 angular.module('portsApp')
-  .controller('DisplayCtrl', ['$scope', 'clientLoaderService',
-    function ($scope, clientLoaderService) {
-        $scope.results = [];
-        $scope.httpStatus = 0;
-        $scope.LoadRequest = clientLoaderService.loadClientList();
-        clientLoaderService.loadClientList()
-            .then(function(results){
-                $scope.httpStatus = 200;
-                $scope.results = results;
-                console.log('###', results);
-              });
+.controller('DisplayCtrl', ['$scope', 'clientLoaderService',
+  function ($scope, clientLoaderService) {
+
+    $scope.results = [];
+    $scope.httpStatus = 0;
+    $scope.LoadRequest = clientLoaderService.loadClientList();
+    clientLoaderService.loadClientList()
+    .then(function(results){
+
+      $scope.httpStatus = 200;
+      $scope.results = results;
+      console.log('###', results);
+    });
   }]);
--- example/scripts/services/clientloader.js
+++ example/scripts/services/clientloader.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('portsApp')
-  .factory('clientLoaderService', [
+.factory('clientLoaderService', [
     '$q', '$http',
     function ($q, $http) {
         var loadClientList = function(){
@@ -12,23 +12,23 @@ angular.module('portsApp')
             var url = 'http://spreadsheets.google.com/feeds/list/' + key + '/' + sheet + '/public/values?alt=json-in-script';
 
             $http.jsonp(url + '&callback=JSON_CALLBACK')
-                .success(function(data){
-                    console.log('!!', data);
-                    var results = [];
-                    var feed = data.feed;
-                    var entries = feed.entry || [];
-                    for (var i=0; i<entries.length; i++){
-                        var value = entries[i];
-                        results.push(value);
-                    }
-                    deferred.resolve(results);
-                })
-                .error(function(reason){
-                    deferred.reject(reason);
-                });
-                return deferred.promise;
-            };
+            .success(function(data){
+                console.log('!!', data);
+                var results = [];
+                var feed = data.feed;
+                var entries = feed.entry || [];
+                for (var i=0; i<entries.length; i++){
+                    var value = entries[i];
+                    results.push(value);
+                }
+                deferred.resolve(results);
+            })
+            .error(function(reason){
+                deferred.reject(reason);
+            });
+            return deferred.promise;
+        };
         return {
             loadClientList: loadClientList
         };
-}]);
+    }]);
